3d56c3a7c390b08efcc54722850f83bf776c5d5f
Denis Woeffray <denis.woeffray@gmail.com>
2022-03-04 10:31:26 +0100

    remove existing gitignore before moving symfony's one

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ then
     echo "Symfony already installed, run command : docker-compose up --build"
 else
     symfony new temp --version=${SYMFONY_VERSION}
+    rm ./.gitignore
     mv ./temp/.gitignore .
     rm -Rf ./.git
     mv ./temp/.git .
